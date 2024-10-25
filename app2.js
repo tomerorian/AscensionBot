9247b6ecd3c90ef5273fd14d3df8e2fb324a229f
tomerorian <tomer.orian@banditos.studio>
2024-10-25 17:21:40 +0300

    Command handling

--- app2.js
+++ app2.js
@@ -47,7 +47,25 @@ client.once(Events.ClientReady, readyClient => {
 
 // Interaction event
 client.on(Events.InteractionCreate, async interaction => {
-    interaction.reply('Pong!');
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
 });
 
 client.on(Events.MessageCreate, async message => {
