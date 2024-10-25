8105e189170203a9d5583329138b5131297d77a5
tomerorian <tomer.orian@banditos.studio>
2024-10-25 15:51:40 +0300

    Tests

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
     GatewayIntentBits.GuildIntegrations
   ],
 });
