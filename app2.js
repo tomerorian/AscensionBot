633658b5b047e3f292e5bbd527338b657115ef7c
tomerorian <tomer.orian@banditos.studio>
2024-10-25 16:38:09 +0300

    New test

--- app2.js
+++ app2.js
@@ -1,16 +1,42 @@
 ﻿import 'dotenv/config';
+import * as fs from 'node:fs';
+import * as path from "node:path";
 
 // Require the necessary discord.js classes
-import { Client, Events, GatewayIntentBits }  from 'discord.js';
+import {Client, Collection, Events, GatewayIntentBits} from 'discord.js';
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, interaction => {
+    console.log(interaction);
 });
 
 // Log in to Discord with your client's token
