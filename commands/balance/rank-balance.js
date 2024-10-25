cfa5186474a235e6605a6a99f85076e93fe39757
tomerorian <tomer.orian@banditos.studio>
2024-10-25 22:51:38 +0300

    balance ranks first attempt

---
+++ commands/balance/rank-balance.js
@@ -0,0 +1,28 @@
+﻿import { SlashCommandBuilder } from 'discord.js';
+import supabase from '../../supabaseClient.js'
+import * as repl from "node:repl";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('rank-balance')
+        .setDescription('Shows the balance rankings.'),
+
+    async execute(interaction) {
+        const balanceRes = await supabase
+            .from('balances')
+            .select('balance,discord_id')
+            .eq('server_id', interaction.guildId)
+            .order('balance', { ascending: false })
+            .limit(10);
+
+        if (balanceRes.error != null) {
+            console.log(balanceRes.error.message);
+
+            return await interaction.reply('An error occurred while trying to get balance rank.');
+        }
+        
+        const reply = balanceRes.data.map(x => `<@${x.discord_id}>: ${x.balance}`).join('\n');
+
+        await interaction.reply(reply);
+    },
+};
