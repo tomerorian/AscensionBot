28c3510e0958e707bf437ce6197f2aa7ce2baa58
tomerorian <tomer.orian@banditos.studio>
2024-10-29 18:10:52 +0200

    Test

---
+++ test.js
@@ -0,0 +1,11 @@
+﻿import Tesseract from 'tesseract.js';
+
+Tesseract.recognize('./example.png', 'eng', {
+    logger: m => console.log(m) // Log progress messages
+})
+    .then(({ data: { text } }) => {
+        console.log('Extracted Text:\n', text);
+    })
+    .catch(err => {
+        console.error('Error:\n', err);
+    });
