7d3b57263a0d746f46e8b7ac1d161f4f7e028225
tomerorian <tomer.orian@banditos.studio>
2024-11-08 11:20:02 +0200

    Attempt to move to local DB using supabase client

---
+++ db.js
@@ -0,0 +1,11 @@
+﻿import { Pool } from 'pg';
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: 'localhost',
+    database: 'albion',
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+});
+
+export default pool;
