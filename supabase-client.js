3f3dec2756ab85a8e2d138c17129616a5ecb3bad
tomerorian <tomer.orian@banditos.studio>
2024-11-08 11:23:32 +0200

    Forgot one file...

--- supabase-client.js
+++ supabase-client.js
@@ -1,8 +1,17 @@
 ﻿import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rthkuqkvbjozjzoabvfh.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY; // Ensure this is set in your environment variables
+//const supabaseUrl = 'https://rthkuqkvbjozjzoabvfh.supabase.co';
+const supabaseUrl = 'http://localhost:5432';
+const supabaseKey = process.env.SUPABASE_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey, {
+    db: {
+        user: process.env.DB_USER,
+        host: 'localhost',
+        database: 'albion',
+        password: process.env.DB_PASSWORD,
+        port: 5432,
+    }
+});
 
 export default supabase;
