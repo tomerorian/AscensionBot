ce622ed1c19263276f803e49d4fe10e1bdc89fd4
tomerorian <tomer.orian@banditos.studio>
2024-11-16 19:28:41 +0200

    party-add an party-members

---
+++ commands/party/party-add.js
@@ -0,0 +1,85 @@
+﻿import { SlashCommandBuilder } from 'discord.js';
+import sql from '../../db.js';
+import roles from "../../roles.js";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('party-add')
+        .setDescription('Adds a member to an existing party.')
+        .addStringOption(option =>
+            option
+                .setName('party')
+                .setDescription('The name of the party')
+                .setRequired(true)
+        )
+        .addUserOption(option =>
+            option
+                .setName('member')
+                .setDescription('The Discord user to add to the party')
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        const partyName = interaction.options.getString('party');
+        const member = interaction.options.getUser('member');
+        const serverId = interaction.guildId;
+        const requesterId = interaction.user.id;
+
+        try {
+            const isAdmin = roles.hasRole(interaction.member, [roles.Admin]);
+            if (!isAdmin) {
+                return await interaction.reply({
+                    content: 'You do not have permission to add members to a party.',
+                    ephemeral: true
+                });
+            }
+            
+            const party = await sql`
+                SELECT id, created_by FROM parties
+                WHERE server_id = ${serverId} AND name = ${partyName}
+            `;
+
+            if (party.length === 0) {
+                return await interaction.reply({
+                    content: `No party with the name "${partyName}" exists in this server.`,
+                    ephemeral: true
+                });
+            }
+            
+            if (party[0].created_by !== requesterId && !isAdmin) {
+                return await interaction.reply({
+                    content: `You do not have permission to add members to the party "${partyName}".`,
+                    ephemeral: true
+                });
+            }
+
+            const memberExists = await sql`
+                SELECT 1 FROM party_members
+                WHERE party_id = ${party[0].id} AND discord_id = ${member.id}
+            `;
+
+            if (memberExists.length > 0) {
+                return await interaction.reply({
+                    content: `<@${member.id}> is already a member of the party "${partyName}".`,
+                    ephemeral: true
+                });
+            }
+
+            await sql`
+                INSERT INTO party_members (party_id, discord_id)
+                VALUES (${party[0].id}, ${member.id})
+            `;
+
+            await interaction.reply({
+                content: `<@${member.id}> has been successfully added to the party "${partyName}".`,
+                ephemeral: true
+            });
+        } catch (error) {
+            console.error(error.message);
+            await interaction.reply({
+                content: 'An error occurred while trying to add the member to the party.',
+                ephemeral: true
+            });
+        }
+    },
+};
---
+++ commands/party/party-members.js
@@ -0,0 +1,57 @@
+﻿import { SlashCommandBuilder } from 'discord.js';
+import sql from '../../db.js';
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('party-members')
+        .setDescription('Lists the members of a party.')
+        .addStringOption(option =>
+            option
+                .setName('party')
+                .setDescription('The name of the party')
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        const partyName = interaction.options.getString('party');
+        const serverId = interaction.guildId;
+
+        try {
+            const party = await sql`
+                SELECT id FROM parties
+                WHERE server_id = ${serverId} AND name = ${partyName}
+            `;
+
+            if (party.length === 0) {
+                return await interaction.reply({
+                    content: `No party with the name "${partyName}" exists in this server.`,
+                    ephemeral: true
+                });
+            }
+
+            const members = await sql`
+                SELECT discord_id FROM party_members
+                WHERE party_id = ${party[0].id}
+            `;
+
+            if (members.length === 0) {
+                return await interaction.reply({
+                    content: `The party "${partyName}" has no members.`,
+                    ephemeral: true
+                });
+            }
+
+            const memberList = members.map(member => `<@${member.discord_id}>`).join('\n');
+            await interaction.reply({
+                content: `Members of "${partyName}":\n${memberList}`,
+                ephemeral: false
+            });
+        } catch (error) {
+            console.error(error.message);
+            await interaction.reply({
+                content: 'An error occurred while trying to retrieve the party members.',
+                ephemeral: true
+            });
+        }
+    },
+};
