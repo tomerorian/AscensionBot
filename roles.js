f2486602fd0928d52a0f0a6be6aa5e38ac2a3e30
tomerorian <tomer.orian@banditos.studio>
2024-11-19 00:28:55 +0200

    multiple roles for role

--- roles.js
+++ roles.js
@@ -5,20 +5,25 @@ const roles = {
     PartyManage: 'PartyManage',
     BalanceManage: 'BalanceManage',
 
-    async getRoleName(serverId, roleKey) {
-        const roleConfig = await sql`
+    async getRoleNames(serverId, roleKey) {
+        const roleConfigs = await sql`
             SELECT role_name FROM roles_config
             WHERE role_key = ${roleKey} AND server_id = ${serverId}
         `;
-        return roleConfig.length > 0 ? roleConfig[0].role_name : null;
+        
+        return roleConfigs.map(config => config.role_name);
     },
 
     async hasRole(member, roleKeys) {
         const serverId = member.guild.id;
-        const roleNames = await Promise.all(
-            roleKeys.map(roleKey => this.getRoleName(serverId, roleKey))
-        );
-        return member.roles.cache.some(r => roleNames.includes(r.name));
+        const roleNamesSet = new Set();
+
+        for (const roleKey of roleKeys) {
+            const roleNames = await this.getRoleNames(serverId, roleKey);
+            roleNames.forEach(name => roleNamesSet.add(name));
+        }
+
+        return member.roles.cache.some(r => roleNamesSet.has(r.name));
     }
 };
 
