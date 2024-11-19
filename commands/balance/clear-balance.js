3b5602c67f77140fcf76f5f8d31bc4fdba5d53b4
tomerorian <tomer.orian@banditos.studio>
2024-11-19 22:51:24 +0200

    log

--- commands/balance/clear-balance.js
+++ commands/balance/clear-balance.js
@@ -2,6 +2,7 @@
 import sql from '../../db.js';
 import roles from "../../roles.js";
 import consts from "../../consts.js";
+import logBalanceChange from "../../log-balance-change.js";
 
 export default {
     data: new SlashCommandBuilder()
@@ -10,7 +11,11 @@ export default {
         .addUserOption(option => option
             .setName('user')
             .setDescription('user to add balance to')
-            .setRequired(true)),
+            .setRequired(true))
+        .addStringOption(option => option
+            .setName('comment')
+            .setDescription('Optional comment for clearing the balance')
+            .setRequired(false)),
 
     async execute(interaction) {
         if (!await roles.hasRole(interaction.member, [roles.Admin, roles.BalanceManage])) {
@@ -18,6 +23,7 @@ export default {
         }
 
         const user = interaction.options.getUser('user');
+        const comment = interaction.options.getString('comment') || null;
 
         let balance = 0;
 
@@ -42,6 +48,15 @@ export default {
                 SET balance = 0
                 WHERE server_id = ${interaction.guildId} AND discord_id = ${user.id}
             `;
+
+            await logBalanceChange({
+                serverId: interaction.guildId,
+                sourceUserId: interaction.user.id,
+                targetUserId: user.id,
+                amount: -balance,
+                reason: 'manual',
+                comment: comment
+            });
 
             await interaction.reply(`<@${interaction.user.id}> cleared ${balance.toLocaleString()} ${consts.CoinEmoji} from <@${user.id}>. New balance is 0 ${consts.CoinEmoji}`);
         } catch (error) {
